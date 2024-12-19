f8846cf8500dbd8a1628504df75e55a92e5bbc69
Andrew Brown <andrew.brown@bom.gov.au>
2024-12-19 16:55:06 +1100

    Copied sea breeze driver functions and coastline angle dirver functions into directories for each model

---
+++ aus2200/aus2200_frontogenesis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -P gb02 
+#PBS -q hugemem
+#PBS -l walltime=6:00:00,mem=512GB 
+#PBS -l ncpus=48
+#PBS -l jobfs=32gb
+#PBS -o /home/548/ab4502/working/ExtremeWind/jobs/messages/aus2200_frontogenesis.o 
+#PBS -e /home/548/ab4502/working/ExtremeWind/jobs/messages/aus2200_frontogenesis.e
+#PBS -l storage=gdata/gb02+gdata/hh5+gdata/ua8
+ 
+export HDF5_USE_FILE_LOCKING=FALSE
+
+#Set up conda/shell environments 
+module use /g/data/hh5/public/modules
+module load conda/analysis3
+module load dask-optimiser
+
+python /home/548/ab4502/working/sea_breeze/aus2200/aus2200_frontogenesis.py "2016-01-01 00:00" "2016-01-31 23:00" --lon_chunk "0" --lat_chunk "0" --time_chunk "0" --model aus2200 --exp_id "mjo-elnino"
